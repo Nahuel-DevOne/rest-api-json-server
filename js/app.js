81e158ccb39719a78a04dcbbf7088f5e21999efe
Nahuel <nahuel.profesor@gmail.com>
2022-11-25 21:05:17 -0300

    Trabajando en mostrar los platillos

--- js/app.js
+++ js/app.js
@@ -44,6 +44,9 @@ function guardarCliente() {
     // Mostrar secciones
     mostrarSecciones();
 
+    // Obtener platillos de la API de JSON-Server.
+    obtenerPlatillos();
+
 }
 
 function mostrarSecciones() {
@@ -51,3 +54,28 @@ function mostrarSecciones() {
     seccionesOcultas.forEach( seccion => seccion.classList.remove('d-none'))
 }
 
+function obtenerPlatillos(){
+    const url = 'http://localhost:4000/platillos';
+    fetch(url)
+        .then( respuesta => respuesta.json())
+        .then( resultado => mostrarPlatillos(resultado))
+        .catch( error => console.log(error))
+}
+
+function mostrarPlatillos(platillos){
+    // console.log(platillos);
+    const contenido = document.querySelector('#platillos .contenido');
+
+    platillos.forEach( platillo => {
+        const row = document.createElement('DIV');
+        row.classList.add('row');
+
+        const nombre = document.createElement('DIV');
+        nombre.classList.add('col-md-4');
+        nombre.textContent = platillo.nombre;
+
+        row.appendChild(nombre);
+        contenido.appendChild(row);
+    })
+    
+}
